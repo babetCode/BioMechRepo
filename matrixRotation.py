2038fe75a809f455a19c771e8ef0e7bdd465d94f
babetCode <adrienbabet1@gmail.com>
2024-09-02 14:18:47 -0600

    update to cartesian vector rotation

--- matrixRotation.py
+++ matrixRotation.py
@@ -1,16 +1,21 @@
 import numpy as np
 from math import *
 
-vector2d = np.array([[1],[0]])
-
+#set angles
 theta = pi/3
+phi = 0
 
+#make a 2d vector & rotation matrix
+vector2d = np.array([[1],[0]])
 rotation_matrix = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
 
 #rotate 2d vector counterclockwise by theta
 def rotate2dvec(vec, theta):
     matrix = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
     return(np.dot(matrix, vec))
+
+#make a 3d vector
+vec3d = np.array([[cos(theta)], [sin(theta)*cos(phi)], [sin(phi)]])
 
 #rotate vector by yaw A, ptich B, roll C
 def rotate3dvec(vec, A, B, C):
@@ -21,19 +26,27 @@ def rotate3dvec(vec, A, B, C):
     return(np.dot(matrix, vec))
 
 #rotate around x, y, z, axes
-def rotateAxes(vec, x, y, z):
+def rotateAxes(vector, x, y, z):
+    vec = vector
+    print(vec, '\n \n')
     r_x = np.array([[1, 0, 0], [0, cos(x), -sin(x)], [0, sin(x), cos(x)]])
     r_y = np.array([[cos(y), 0, sin(y)], [0, 1, 0], [-sin(y), 0, cos(y)]])
     r_z = np.array([[cos(z), -sin(z), 0], [sin(z), cos(z), 0], [0, 0, 1]])
-    print(r_x, '\n \n', r_y, '\n \n', r_z)
+    for i in [r_x, r_y, r_z]:
+        vec = np.dot(i, vec)
+        print(vec, '\n \n')
+
 
 #convert spherical coordinates to cartesian coordinates
+def sphere2cart():
+    print('empty')
 
-
-#create main function
+"""
+DEFINE MAIN FUNCTION
+"""
 def main():
 
-    rotateAxes(1, 1, 1, 1)    
+    rotateAxes(vec3d, pi, pi, pi/6)    
 
 #call main
 if __name__ == "__main__":
