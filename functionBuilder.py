33b54b2c9e3c4afa87f051a8dc908f5f75bb96ec
Adrien Babet <adrienbabet1@gmail.com>
2024-09-03 12:11:29 -0600

    Update functionBuilder.py with quaternion rotation function

--- functionBuilder.py
+++ functionBuilder.py
@@ -1,4 +1,42 @@
-""
+"""
 A drafting space for functions used in IMU gait analysis
+"""
 
-""
+import numpy as np
+from math import *
+
+#multiply  two quaternions, given as 4-item lists
+def multiplyQuaternion(q1, q2):
+    [w1, x1, y1, z1] = [value for value in q1]
+    [w2, x2, y2, z2] = [value for value in q2]
+    scalar = w1*w2 - x1*x2 - y1*y2 - z1*z2
+    i = w1*x2 + x1*w2 + y1*z2 - z1*y2
+    j = w1*y2 + y1*w2 + z1*x2 - x1*z2
+    k = w1*z2 + z1*w2 + x1*y2 -y1*x2
+    return([scalar, i, j, k])
+
+#given point as 3-item list, angle in radians, axix as 3-item list
+def rotateQuaternion(point, angle, axis):
+    normalizedAxis = axis/np.linalg.norm(axis)
+    q_inv = [cos(angle/2)]
+    q = [cos(angle/2)]
+    q_inv = [cos(angle/2)]
+    for value in normalizedAxis:
+        q.append(sin(angle/2)*value)
+        q_inv.append(-sin(angle/2)*value)
+    pointQuat = [0, point[0], point[1], point[2]]
+    rotation = multiplyQuaternion(multiplyQuaternion(q, pointQuat), q_inv)
+    result = [float(i) for i in rotation[1:]]
+    roundedPoint = [round(i, 3) for i in point]
+    roundedAngle = round(angle, 3)
+    roundedAxis = [round(i, 3) for i in axis]
+    roundedResult = [round(i, 3) for i in result]
+    print('the rotation of ', roundedPoint, ' by ', roundedAngle, ' radians around ', roundedAxis, ' is ', roundedResult, '\n')
+    return(result)
+
+def main():
+    rotateQuaternion([1.000,0.000,0.000], pi, [0,sqrt(2)/2,sqrt(2)/2])
+
+#CALL MAIN
+if __name__ == '__main__':
+    main()
