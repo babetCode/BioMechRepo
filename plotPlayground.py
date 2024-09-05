d4f7d2a1e82d4ae3e1f7dba49aed929035812368
babetCode <adrienbabet1@gmail.com>
2024-09-04 21:33:22 -0600

    changed data analysis to work with my windows machine file directory

--- plotPlayground.py
+++ plotPlayground.py
@@ -6,3 +6,4 @@ default_orientation = np.array([[1,0,0], [0,1,0], [0,0,1]])
 
 print([[f"test({j})" for  j in i] for i in default_orientation])
 
+#C:/Users/goper/Files/vsCode/490R/Walking_C3D_files/
