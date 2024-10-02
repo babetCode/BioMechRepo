08b5d975adb4431bfe52d14709ee0f4298c73a4b
babetCode <adrienbabet1@gmail.com>
2024-10-02 15:19:44 -0600

    .

--- AdriensTesting/Playgrounds/plots.py
+++ AdriensTesting/Playgrounds/plots.py
@@ -4,11 +4,14 @@ from imufunctions import*
 
 mypath = adrienC3Dpath()
 df = c3d_analogs_df('C07', 'Fast', '07', mypath)
+print(df)
 rdshank = imu('right_distal_shank', df, 9)
 rfoot = imu('right_foot', df, 10)
 gravity = np.ones(rdshank.frames)
-
+plt.close('all')
 plt.plot(gravity, label='gravity')
-plt.plot(rdshank.net_acc)
-plt.plot(rfoot.net_acc)
-plt.show()
+plt.plot(rdshank.net_acc, label='rdshank')
+plt.plot(rfoot.net_acc, label='rfoot')
+plt.legend()
+plt.show(block=False)
+keepfigure = input('ENTER to close plot')
