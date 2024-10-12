fc0f741c6631e1209cc342aa9f45a33775f78fef
babetCode <adrienbabet1@gmail.com>
2024-10-12 10:59:24 -0600

    .

--- AdriensTesting/Playgrounds/adriensdir.py
+++ AdriensTesting/Playgrounds/adriensdir.py
@@ -13,29 +13,11 @@ class BioMechDir:
     def __str__(self):
         return f'{self.machine}'
     
-    def getmyfunctions(self):
+    def getimufunctions(self):
         path2functions = {'abpc': 'c:/Users/goper/Files/vsCode/490R/BioMechRepo',
             'abmac': '/Users/adrienbabet/Documents/vsCode/490R/BioMechRepo',
             'tmlaptop': 'C:/Users/tm4dd/Documents/00_MSU/01_PhD_Research/Python_code'}
         sys.path.insert(0, f'{path2functions[self.machine]}')
-        print(f'added "{path2functions[self.machine]}" to sys path')
-        print('ready to import imufunctions')      
+        return f'{self.machine}'
 
-# myDir = BioMechDir()
-
-# myDir.getmyfunctions()
-
-# from imufunctions import *
-
-# my3dplot = plt.figure().add_subplot(projection='3d')
-
-# my3dplot.set_xlabel('x')
-
-# my3dplot.set_ylabel('y')
-
-# my3dplot.set_zlabel('z')
-
-# plot3axes(my3dplot)
-
-# plt.show()
 
--- AdriensTesting/Playgrounds/test.py
+++ AdriensTesting/Playgrounds/test.py
@@ -1,5 +1,15 @@
 from adriensdir import BioMechDir
-mydir = BioMechDir()
-mydir.getmyfunctions()
+mydir = BioMechDir().getimufunctions()
 from imufunctions import *
-print(str(mydir))
+print(mydir)
+my3dplot = plt.figure().add_subplot(projection='3d')
+
+my3dplot.set_xlabel('x')
+
+my3dplot.set_ylabel('y')
+
+my3dplot.set_zlabel('z')
+
+plot3axes(my3dplot)
+
+plt.show()
