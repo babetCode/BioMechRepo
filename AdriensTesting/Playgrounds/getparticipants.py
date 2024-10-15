c610cdfa356cf134fba87d2b4ab4d7134da958ae
babetCode <adrienbabet1@gmail.com>
2024-10-15 09:08:27 -0600

    .

--- AdriensTesting/Playgrounds/getparticipants.py
+++ AdriensTesting/Playgrounds/getparticipants.py
@@ -21,32 +21,40 @@ pars = []
 
 tris = []
 
-for n, par_folder_path in enumerate(glob.glob(f'{myfns.adrien_c3d_folder(mydir)}/*')):
+paths = []
+
+for n, par_folder_path in enumerate(glob.glob(f'{myfns.adrien_c3d_folder(mydir)}\\*')):
     participant = par_folder_path.rsplit('\\', 1)[-1][:3]
     pars.append(participant)
     session = []
+    parpaths = []
     for mocap_run in glob.glob(f'{par_folder_path}/*'):
+        parpaths.append(mocap_run.replace('\\','/'))
         run_id = mocap_run.rsplit('\\', 1)[-1].removesuffix('.c3d')[4:]
         session.append(run_id)
+    paths.append(parpaths)
     tris.append(session)
 
 maxlen = max(len(i) for i in tris)
 
-print(f'max len {maxlen}')
+print(f'paths {len(paths)} tris {len(tris)}')
+print(f'{myfns.adrien_c3d_folder(mydir)}')
 
 for trial in tris:
     if len(trial) < maxlen:
         for i in range(maxlen - len(trial)):
             trial.append('n/a')
 
+for path in paths:
+    if len(path) < maxlen:
+        for i in range(maxlen - len(path)):
+            path.append('n/a')
+
 
 mydata = dict(zip(pars, tris))
 
-df = pd.DataFrame(mydata)
+fulldata = dict(zip(pars, paths))
 
-print(df)
+df = pd.DataFrame(fulldata)
 
-
-
-
-
+df.to_csv('C:\\Users\\goper\\Files\\vsCode\\490R\\mocappaths.csv')
